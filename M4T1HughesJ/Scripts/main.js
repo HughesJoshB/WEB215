916b95e4b90bce90401b9001b04cb9b02972443b
HughesJoshB <31249365+HughesJoshB@users.noreply.github.com>
2017-11-09 11:50:36 -0500

    registration page update

---
+++ M4T1HughesJ/Scripts/main.js
@@ -0,0 +1,13 @@
+// Javascript file for hiding alerts
+// WEB215 Hughes, Josh
+//11/9/2017
+
+/*$('#hide').click(function(){
+	$(')
+}*/
+
+$(function()
+{  
+$('div [class*="alert"]').text('This works!')
+	//alert('Hello World');
+})
