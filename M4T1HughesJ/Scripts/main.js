4d6c7c9884909ac9c89a478bc384da191589ac1b
HughesJoshB <31249365+HughesJoshB@users.noreply.github.com>
2017-11-16 11:43:36 -0500

    validation
    Worked on form validation for registration.html and main.js

--- M4T1HughesJ/Scripts/main.js
+++ M4T1HughesJ/Scripts/main.js
@@ -9,8 +9,30 @@
 
 $(function()
 {  
+//hide alerts on load
 $('div [class*="alert-success"]').addClass('hidden');
 $('div [class*="alert-danger"]').addClass('hidden');
+
+$('#submit').click(function(){
+	//submit click handler
+	//first pass just show alerts
 	
+	//$('div [class*="alert-success"]').removeClass('hidden');
+	$('input').each(function(){
+		//use HTML5 checkValidity function
+		var inputGroup = $(this).parents('.input-group');
+		
+		if (this.checkValidity()){
+			//$('div [class*="alert-success"]').removeClass('hidden');
+			inputGroup.addClass('has-success').removeClass('has-error');
+			//this.addClass('has-success').removeClass('has-error');
+		}
+		else{
+			$('div [class*="alert-danger"]').removeClass('hidden');
+			inputGroup.addClass('has-error').removeClass('has-success');
+			//this.addClass('has-error').removeClass('has-success');
+		}
+	})
+	})
 })
 
